ea0d209788e2f9271d7986df59df9d505b70c19a
GB Krishna <GB Krishna@GBKrishna.mshome.net>
2020-11-23 23:19:39 +0530

    spring first commits

---
+++ src/main/java/com/comp/HelloController.java
@@ -0,0 +1,15 @@
+package com.comp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+	
+	@RequestMapping("/")  
+    public String display()  
+    {  
+        return "index";  
+    }     
+
+}
